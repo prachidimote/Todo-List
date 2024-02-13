894b2a9a4fa384df49a51bfe209c24c5e9da2866
prachidimote <81797753+prachidimote@users.noreply.github.com>
2024-02-13 16:56:43 +0530

    UI enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,77 @@
+import './App.css';
+import { useState } from 'react';
+import { FaCircleCheck } from "react-icons/fa6";
+import { MdEdit } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
 const App = () => {
+  const [todos, setTodos] = useState([ 
+    {id: 1, title: "Task 1", status: true},
+    {id: 2, title: "Task 2", status: false},
+  ]);
+  //state to add new tasks
+  const [newTask, setNewTask] = useState('');
+  
+  //state for editing the data
+  const [updateData, setUpdateData] = useState('');
+
+  //Add task
+  const handleAddTask = () => {
+    //
+  }
+
+  //Edit task
+  const handleEdit = () => {
+    //
+  }
+
+  //Delete task
+  const handleDelete = (id) => {
+    //
+  }
+
+  //Mark task as done or completed
+  const handleMark = (id) => {
+    //
+  }
+
+  //Change task for update
+  const handleCancelUpdate = () => {
+    //
+  }
+
+  //Change task for update
+  const handleChangeTask = (e) => {
+    //
+  }
+
+  //update task
+  const handleUpdateTask = () => {
+    //
+  }
+
   return (
-  <div>
-      <h1 className="text-3xl font-bold underline mt-40 text-center">
-      Hello world!
-    </h1>
+  <div className='App'>
+      <h2>TODO LIST APP</h2>
+      {todos && todos.length ? '' : 'No Tasks'}
+      {
+        todos.map((task, index) => {
+          return(
+            <div key={task.id}>
+            <div className='col taskbg'>
+            <div className={task.status ? 'done' : ''}>
+            <span className='taskNumber'>{index + 1}</span>
+            <span className='taskText'>{task.title}</span>
+            </div>
+            <div className='iconsWrap'>
+              <span><FaCircleCheck/></span>
+              <span><MdEdit /></span>
+              <span><MdDeleteForever /></span>
+            </div>
+            </div>
+            </div>
+          )
+        })
+      }
   </div>
   )
 }
