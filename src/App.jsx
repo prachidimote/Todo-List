cb8335c8f4be2e9513f76401a380f0d0c5573936
prachidimote <81797753+prachidimote@users.noreply.github.com>
2024-02-17 17:53:14 +0530

    edit and update todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { MdDeleteForever } from "react-icons/md";
 const App = () => {
   const [todos, setTodos] = useState([ 
     {id: 2, title: "Task 2", status: false},
-    {id: 1, title: "Task 1", status: true},
+    {id: 1, title: "Task 1", status: false},
   ]);
   //state to add new tasks
   const [newTask, setNewTask] = useState('');
@@ -50,17 +50,25 @@ const App = () => {
 
   //Change task for update
   const handleCancelUpdate = () => {
-    //
+    setUpdateData('');
   }
 
   //Change task for update
   const handleChangeTask = (e) => {
-    //
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false
+    }
+    setUpdateData(newEntry);
   }
 
   //update task
   const handleUpdateTask = () => {
-    //
+    let filteredRecords = [...todos].filter(task => task.id !== updateData.id);
+    let updatedObject = [...filteredRecords, updateData];
+    setTodos(updatedObject);
+    setUpdateData('');
   }
 
   const changeInput = (event) => {
@@ -74,9 +82,15 @@ const App = () => {
       <h2>TODO LIST APP</h2>
       <div className='row'>
       <div>
-    <input type="text" id="small-input" className="block w-full p-2  border-none rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+    <input
+    value={updateData && updateData.title}
+    onChange={(e) => handleChangeTask(e)} 
+    type="text" 
+    id="small-input" 
+    className="block w-full p-2  border-none rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+    <button 
+    onClick={handleUpdateTask}
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
   Update 
 </button>
 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
@@ -98,7 +112,6 @@ const App = () => {
 </button>
 
 </div>
-
       </div>
       {todos && todos.length ? '' : 'No Tasks'}
       {
@@ -117,7 +130,15 @@ const App = () => {
               onClick={() => handleMarkDone(task.id)}
               title='Completed/Not Completed'
               ><FaCircleCheck/></span>
-              <span title='Edit'><MdEdit /></span>
+            {task.status ? null : (
+                <span 
+                onClick={() => setUpdateData({
+                  id: task.id, 
+                  title: task.title,
+                  status: task.status ? true : false
+                })}
+                title='Edit'><MdEdit /></span>
+            )}
               <span title='Delete' onClick={() => handleDelete(task.id)}><MdDeleteForever /></span>
             </div>
             </div>
